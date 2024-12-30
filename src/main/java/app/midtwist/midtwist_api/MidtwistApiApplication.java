016f1fbf292c5f860aa2cea224957305dd6e67c4
Denilson <denilsob.b.sousa2021@gmail.com>
2024-12-30 09:45:14 -0300

    chore: initial project setup

---
+++ src/main/java/app/midtwist/midtwist_api/MidtwistApiApplication.java
@@ -0,0 +1,13 @@
+package app.midtwist.midtwist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtwistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtwistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/midtwist/midtwist_api/MidtwistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.midtwist.midtwist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtwistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
